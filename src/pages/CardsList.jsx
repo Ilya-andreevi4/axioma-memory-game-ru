5cd7283297296b58f804ea1dd569b52b7891c3a2
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2022-10-07 16:12:11 +0600

    изменено:      package-lock.json 	изменено:      src/index.css 	изменено:      src/pages/CardsList.jsx 	изменено:      src/pages/GamePage.jsx

--- src/pages/CardsList.jsx
+++ src/pages/CardsList.jsx
@@ -1,87 +1,183 @@
 import React from "react";
-import { useState, useCallback, useMemo, useEffect } from 'react';
-import { Settings } from './setting/Settings'
-import { Link } from 'react-router-dom'
+import { useState, useCallback, useMemo, useEffect } from "react";
+import { Settings } from "./setting/Settings";
+import { Link } from "react-router-dom";
 
 const getData = () => [
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/bee.jpg", name: "bee" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/bird.jpg", name: "bird" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/butterfly.jpg", name: "butterfly" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/chamomile.jpg", name: "chamomile" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/flower.jpg", name: "flower" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/flower1.jpg", name: "flower1" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/ladybug.jpg", name: "ladybug" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/snail.jpg", name: "snail" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/tree.jpg", name: "tree" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/bee.jpg", name: "bee" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/bird.jpg", name: "bird" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/chamomile.jpg", name: "chamomile" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/butterfly.jpg", name: "butterfly" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/flower.jpg", name: "flower" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/flower1.jpg", name: "flower1" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/ladybug.jpg", name: "ladybug" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/snail.jpg", name: "snail" },
-    { back: "img/cards/card_logo.png", imgSrc: "img/cards_part_1/tree.jpg", name: "tree" }
-]
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/bee.jpg",
+    name: "bee",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/bird.jpg",
+    name: "bird",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/butterfly.jpg",
+    name: "butterfly",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/chamomile.jpg",
+    name: "chamomile",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/flower.jpg",
+    name: "flower",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/flower1.jpg",
+    name: "flower1",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/ladybug.jpg",
+    name: "ladybug",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/snail.jpg",
+    name: "snail",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/tree.jpg",
+    name: "tree",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/bee.jpg",
+    name: "bee",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/bird.jpg",
+    name: "bird",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/chamomile.jpg",
+    name: "chamomile",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/butterfly.jpg",
+    name: "butterfly",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/flower.jpg",
+    name: "flower",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/flower1.jpg",
+    name: "flower1",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/ladybug.jpg",
+    name: "ladybug",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/snail.jpg",
+    name: "snail",
+  },
+  {
+    back: "img/cards/card_logo.png",
+    imgSrc: "img/cards_part_1/tree.jpg",
+    name: "tree",
+  },
+];
 
 const randomize = () => {
-    const cardData = getData();
-    cardData.sort(() => Math.random() - 0.5);
-    return cardData;
+  const cardData = getData();
+  cardData.sort(() => Math.random() - 0.5);
+  return cardData;
 };
 
 export default function CardsList(props) {
-    const cardsData = useMemo(() => randomize(), []);
-    const [isComplete, setIsComplete] = useState(false);
-    const [openedCards, setOpenedCards] = useState(new Map());
-    const [resolvedCards, setResolvedCards] = useState(new Map());
-    const [modalActive, setModalActive] = useState(true);
-    const [isLoading, setIsLoading] = useState(false);
+  const cardsData = useMemo(() => randomize(), []);
+  const [isComplete, setIsComplete] = useState(false);
+  const [openedCards, setOpenedCards] = useState(new Map());
+  const [resolvedCards, setResolvedCards] = useState(new Map());
+  const [modalActive, setModalActive] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  // useEffect(()=>{
 
-    useEffect(() => {
-        if (openedCards.size === 2) {
-            setIsLoading(true);
-            setTimeout(() => {
-                const openedCardsKeys = Array.from(openedCards.keys());
-                if (cardsData[openedCardsKeys[0]].name === cardsData[openedCardsKeys[1]].name) {
-                    let resolvedCardsUpdated = resolvedCards.set(openedCardsKeys[0], true);
-                    resolvedCardsUpdated = resolvedCardsUpdated.set(openedCardsKeys[1], true);
-                    setResolvedCards(new Map(resolvedCardsUpdated));
-                    setIsComplete(resolvedCardsUpdated.size === 18);
-                }
-                setOpenedCards(new Map());
-                setIsLoading(false);
-            }, 1000);
+  // },[])
+  useEffect(() => {
+    if (openedCards.size === 2) {
+      setIsLoading(true);
+
+      setTimeout(() => {
+        const openedCardsKeys = Array.from(openedCards.keys());
+        if (
+          cardsData[openedCardsKeys[0]].name ===
+          cardsData[openedCardsKeys[1]].name
+        ) {
+          let resolvedCardsUpdated = resolvedCards.set(
+            openedCardsKeys[0],
+            true
+          );
+          resolvedCardsUpdated = resolvedCardsUpdated.set(
+            openedCardsKeys[1],
+            true
+          );
+          setResolvedCards(new Map(resolvedCardsUpdated));
+          setIsComplete(resolvedCardsUpdated.size === 18);
+          setOpenedCards(new Map());
+          setIsLoading(false);
+        } else {
+          setOpenedCards(new Map());
+          setIsLoading(false);
         }
-    }, [openedCards, cardsData, resolvedCards, setOpenedCards]);
+      }, 1000);
+    }
+  }, [openedCards, cardsData, resolvedCards, setOpenedCards]);
 
-    const openCard = useCallback((idx) => {
-        if (isLoading) {
-            return
-        } else if (openedCards.size < 2) {
-            setOpenedCards(new Map(openedCards.set(idx, true)));
-        }
-    }, [setOpenedCards, openedCards, isLoading]);
+  const openCard = useCallback(
+    (e, idx) => {
+      if (openedCards.size > 1) {
+        return e.stopPropagation();
+      }
+      if (isLoading) {
+        return e.stopPropagation();
+      } else if (openedCards.size < 2) {
+        setOpenedCards(new Map(openedCards.set(idx, true)));
+      }
+    },
+    [setOpenedCards, openedCards, isLoading]
+  );
 
-    return !isComplete ?
-        <div className={props.className}>
-            {cardsData.map((card, idx) => {
-                return (
-                    <div key={idx}>
-                        <img alt=" "
-                            src={openedCards.get(idx) ? card.imgSrc : card.back}
-                            className={'cardImg'}
-                            style={{ display: resolvedCards.get(idx) ? 'none' : 'block' }}
-                            onClick={() => openCard(idx)}
-                        />
-                    </div>
-                )
-            })}
-        </div>
-        : <Settings active={modalActive} setActive={setModalActive}>
-            <p>Поздравляю! Вы открыли все пары карт!</p>
-            <Link to="./StartPage" className="quitButton">
-                <span className="textQuit">Повторить</span>
-            </Link>
-        </Settings>;
+  return !isComplete ? (
+    <div className={props.className}>
+      {cardsData.map((card, idx) => {
+        return (
+          <div key={idx}>
+            <img
+              alt=""
+              src={openedCards.get(idx) ? card.imgSrc : card.back}
+              className={"cardImg"}
+              style={{ display: resolvedCards.get(idx) ? "none" : "block" }}
+              onClick={(e) => openCard(e, idx)}
+            />
+          </div>
+        );
+      })}
+    </div>
+  ) : (
+    <Settings active={modalActive} setActive={setModalActive}>
+      <p>Поздравляю! Вы открыли все пары карт!</p>
+      <Link to="./StartPage" className="quitButton">
+        <span className="textQuit">Повторить</span>
+      </Link>
+    </Settings>
+  );
 }
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,55 +1,66 @@
-import React from 'react'
-import { Settings } from './setting/Settings'
-import { useState } from 'react'
-import CardsList from './CardsList'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Settings } from "./setting/Settings";
+import { useState } from "react";
+import CardsList from "./CardsList";
+import { Link } from "react-router-dom";
 
 const GamePage = () => {
-    // const players = [
-    //     { id: 1, turn: true, points: 0, title: 'Player 1'},
-    //     { id: 2, turn: false, points: 0, title: 'Player 2'},
-    //     { id: 3, turn: false, points: 0, title: 'Player 3'},
-    //     { id: 4, turn: false, points: 0, title: 'Player 4'}
-    // ]
+  // const [isLoading, setIsLoading] = useState(false);
+  // function loading() {
+  //   setIsLoading(true);
+  // }
+  // document.addEventListener("DOMContentLoaded", loading());
+  // window.onload = () => {
+  //   setIsLoading(false);
+  // };
+  // const players = [
+  //     { id: 1, turn: true, points: 0, title: 'Player 1'},
+  //     { id: 2, turn: false, points: 0, title: 'Player 2'},
+  //     { id: 3, turn: false, points: 0, title: 'Player 3'},
+  //     { id: 4, turn: false, points: 0, title: 'Player 4'}
+  // ]
 
-    const [modalActive, setModalActive] = useState(false);
+  const [modalActive, setModalActive] = useState(false);
 
-    return (
-        <div className="gamePage">
-
-            <div className="playersScores">
-                <button className="settingsImg" onClick={() => setModalActive(true)}>
-                    <img src="img/setting.png" alt="Settings" className="settingIcon" />
-                </button>
-                <div className="playerBox">
-                    <p>Игрок 1</p>
-                    <span className='playerOneScores'></span>
-                </div>
-                <div className="playerBox">
-                    <p>Игрок 2</p>
-                    <span className='playerTwoScores'></span>
-                </div>
-                <div className="playerBox">
-                    <p>Игрок 3</p>
-                    <span className='playerThreeScores'></span>
-                </div>
-                <div className="playerBox">
-                    <p>Игрок 4</p>
-                    <span className='playerFourScores'></span>
-                </div>
-            </div>
-
-            <CardsList className="cardsList" />
-
-            <Settings active={modalActive} setActive={setModalActive}>
-                <h1>Настройки</h1>
-                <Link to="./StartPage" className="quitButton" >
-                    <p className="textQuit">Выход</p>
-                </Link>
-            </Settings>
-
+  return (
+    <div className="gamePage">
+      {/* {isLoading && (
+        <div className="loadingPage">
+          <h1>Идёт загрузка</h1>
         </div>
-    )
-}
+      )} */}
+      <div className="playersScores">
+        <button className="settingsImg" onClick={() => setModalActive(true)}>
+          <img src="img/setting.png" alt="Settings" className="settingIcon" />
+        </button>
+        <div className="playerBox">
+          <p>Игрок 1</p>
+          <span className="playerOneScores"></span>
+        </div>
+        <div className="playerBox">
+          <p>Игрок 2</p>
+          <span className="playerTwoScores"></span>
+        </div>
+        <div className="playerBox">
+          <p>Игрок 3</p>
+          <span className="playerThreeScores"></span>
+        </div>
+        <div className="playerBox">
+          <p>Игрок 4</p>
+          <span className="playerFourScores"></span>
+        </div>
+      </div>
 
-export { GamePage }
+      <CardsList className="cardsList" />
+
+      <Settings active={modalActive} setActive={setModalActive}>
+        <h1>Настройки</h1>
+        <Link to="./StartPage" className="quitButton">
+          <p className="textQuit">Выход</p>
+        </Link>
+      </Settings>
+    </div>
+  );
+};
+
+export { GamePage };
