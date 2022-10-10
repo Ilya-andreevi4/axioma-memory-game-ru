a6f7c70554bb60e7c33d598c0197cfd23fa01988
Ilya-andreevi4 <ilya-andreevi4@mail.ru>
2022-10-10 14:35:43 +0600

    новый файл:    public/img/cards/card_logo_adobe_express.svg 	новый файл:    public/img/cards_part_1/bee_adobe_express.svg 	новый файл:    public/img/cards_part_1/bird_adobe_express.svg 	новый файл:    public/img/cards_part_1/butterfly_adobe_express.svg 	новый файл:    public/img/cards_part_1/chamomile_adobe_express.svg 	новый файл:    public/img/cards_part_1/flower1_adobe_express.svg 	новый файл:    public/img/cards_part_1/flower_adobe_express.svg 	новый файл:    public/img/cards_part_1/ladybug_adobe_express.svg 	новый файл:    public/img/cards_part_1/snail_adobe_express.svg 	новый файл:    public/img/cards_part_1/tree_adobe_express.svg 	изменено:      src/index.css 	изменено:      src/pages/CardsList.jsx 	изменено:      src/pages/StartPage.jsx

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// const checkImage = path =>
+//     new Promise(resolve => {
+//         const img = new Image()
+//         img.onload = () => resolve(path)
+//         img.onerror = (e) => console.error(e);
+//         img.src = path
+//     })
+
 const StartPage = () => {
+//   useEffect(() => {
+//     if(!localStorage.length){
+// localStorage.setItem('img', JSON.stringify)
+//     }
+//     return () => {
+//       cleanup
+//     };
+//   }, []);
+
   return (
     <div className="startpage">
       <header>
